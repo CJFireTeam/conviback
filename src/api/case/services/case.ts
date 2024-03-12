87a3611331fa5fb17bb28ef1849cd7d7f84be534
Cesar Lara <celara@imed.cl>
2024-03-12 15:58:13 -0300

    schema reupdated

--- src/api/case/services/case.ts
+++ src/api/case/services/case.ts
@@ -2,6 +2,37 @@
  * case service
  */
 
-import { factories } from '@strapi/strapi';
+// import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::case.case');
+// export default factories.createCoreService('api::case.case');
+
+const { createCoreController } = require('@strapi/strapi').factories;
+module.exports = createCoreController('api::case.case', ({ strapi }) => ({
+  async find(ctx) {
+    // Calling the default core action
+    const { data, meta } = await super.find(ctx);
+    const query = strapi.db.query('api::case.case');
+    await Promise.all(
+      data.map(async (item, index) => {
+        const foundItem = await query.findOne({
+          where: {
+            id: item.id,
+          },
+          populate: ['createdBy', 'updatedBy'],
+        });
+
+        data[index].attributes.createdBy = {
+          id: foundItem.createdBy.id,
+          firstname: foundItem.createdBy.firstname,
+          lastname: foundItem.createdBy.lastname,
+        };
+        data[index].attributes.updatedBy = {
+          id: foundItem.updatedBy.id,
+          firstname: foundItem.updatedBy.firstname,
+          lastname: foundItem.updatedBy.lastname,
+        };
+      })
+    );
+    return { data, meta };
+  },
+}));
