f7b55dbf65a2b0387569627665770a55d193cc1e
Cesar Lara <celara@imed.cl>
2024-03-27 12:38:42 -0300

    undo cases

--- src/api/case/controllers/case.ts
+++ src/api/case/controllers/case.ts
@@ -1,31 +1,8 @@
-// export default factories.createCoreService('api::case.case');
-const { createCoreController } = require('@strapi/strapi').factories;
-module.exports = createCoreController('api::case.case', ({ strapi }) => ({
-  async find(ctx) {
-    // Calling the default core action
-    const { data, meta } = await super.find(ctx);
-    const query = strapi.db.query('api::case.case');
-    await Promise.all(
-      data.map(async (item, index) => {
-        const foundItem = await query.findOne({
-          where: {
-            id: item.id,
-          },
-          populate: ['createdBy', 'updatedBy'],
-        });
+/**
+ * case controller
+ */
 
-        data[index].attributes.createdBy = {
-          id: foundItem.createdBy.id,
-          firstname: foundItem.createdBy.firstname,
-          lastname: foundItem.createdBy.lastname,
-        };
-        data[index].attributes.updatedBy = {
-          id: foundItem.updatedBy.id,
-          firstname: foundItem.updatedBy.firstname,
-          lastname: foundItem.updatedBy.lastname,
-        };
-      })
-    );
-    return { data, meta };
-  },
-}));
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::case.case');
+
