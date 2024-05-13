4aa1b927b006b12e82a7cc0561596c1eca5a605b
Cesar Lara <celara@imed.cl>
2024-05-13 15:06:53 -0400

    send email in create caso

--- src/api/case/controllers/case.ts
+++ src/api/case/controllers/case.ts
@@ -7,10 +7,17 @@ import { factories } from '@strapi/strapi'
 // export default factories.createCoreController('api::case.case');
 export default factories.createCoreController('api::case.case', ({ strapi }) => ({
   async create(ctx) {
-    // some logic here
+    const { user } = ctx.state;
+    const { body } = ctx.request
     const response = await super.create(ctx);
-    // some more logic
-    console.log(ctx.state.user,response)
+    const url =strapi.config.get('server.url', 'defaultValueIfUndefined')
+    await strapi.plugin('email').service('email').send({
+      to: user.email,
+      subject: 'Nueva Denuncia generada',
+      html: `<p><strong>Hola ${user.firstname} ${user.first_lastname},</strong></p>
+      <p>Su nueva denuncia ha sido registrada.</p>
+      <p>Puede ver su registro en nuestra aplicación <a href=${url}>aquí</a>.</p>`,
+    });
     return response;
   }
 }));
